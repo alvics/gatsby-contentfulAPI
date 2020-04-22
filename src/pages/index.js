1a486af16a701d1032bdc04217fb7079a25d3f27
Allen <alvics71@hotmail.com>
2020-04-22 11:36:38 +1000

    change: remove page 2 link for products page

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,12 +10,13 @@ const IndexPage = () => (
     <SEO title="Home" />
 
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>Welcome to Gatsby with Contenful backend.</p>
+    <p>to be continued ...</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+
+    <Link to="/products/">VIsit our Products</Link>
   </Layout>
 )
 
